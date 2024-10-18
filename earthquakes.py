f60178479d19081a962ef1a86128c1d5ef122fd7
Sara Pozas <Sara@MacBook-Air-2.local>
2024-10-18 12:28:08 +0100

    UCL-COMP0233-24-25/RSE-Classwork#9

--- earthquakes.py
+++ earthquakes.py
@@ -1,61 +1,57 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
 import requests
-
+import json
 
 def get_data():
-    # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
+    """Fetch earthquake data from the USGS web service and save it to a JSON file."""
     response = requests.get(
         "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
         params={
-            'starttime': "2000-01-01",
-            "maxlatitude": "58.723",
-            "minlatitude": "50.008",
-            "maxlongitude": "1.67",
-            "minlongitude": "-9.756",
-            "minmagnitude": "1",
-            "endtime": "2018-10-11",
-            "orderby": "time-asc"}
+            'starttime': "1924-01-01",  # 100 years ago
+            'maxlatitude': "58.723",
+            'minlatitude': "50.008",
+            'maxlongitude': "1.67",
+            'minlongitude': "-9.756",
+            'minmagnitude': "1",
+            'endtime': "2024-01-01",
+            'orderby': "time-asc"
+        }
     )
 
-    # The response we get back is an object with several fields.
-    # The actual contents we care about are in its text field:
-    text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
+    # Save the response text to a JSON file
+    with open('earthquake_data.json', 'w') as json_file:
+        json_file.write(response.text)  # Write the raw response to a file
 
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
+    # Parse the JSON response to a Python dictionary
+    return response.json()
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
-
+    return len(data['features'])  # The number of earthquake entries
 
 def get_magnitude(earthquake):
-    """Retrive the magnitude of an earthquake item."""
-    return ...
-
+    """Retrieve the magnitude of an earthquake item."""
+    return earthquake['properties']['mag']  # Magnitude from properties
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
-    # There are three coordinates, but we don't care about the third (altitude)
-    return ...
-
+    return earthquake['geometry']['coordinates'][0:2]  # First two values are latitude and longitude
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    max_magnitude = float('-inf')  # Start with the lowest possible magnitude
+    max_location = None
 
+    for earthquake in data['features']:
+        magnitude = get_magnitude(earthquake)
+        if magnitude > max_magnitude:
+            max_magnitude = magnitude
+            max_location = get_location(earthquake)  # Update max location if a stronger quake is found
 
-# With all the above functions defined, we can now call them and get the result
-data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
-max_magnitude, max_location = get_maximum(data)
-print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
+    return max_magnitude, max_location
+
+# Main execution
+if __name__ == "__main__":
+    data = get_data()
+    print(f"Loaded {count_earthquakes(data)} earthquakes.")
+    max_magnitude, max_location = get_maximum(data)
+    print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}.")
