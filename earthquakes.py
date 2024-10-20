52916e7ee0a26345d53b7a9aa22ce009444aa8c4
Charlotte Savage <charlotte.e.savage@gmail.com>
2024-10-20 22:55:31 +0100

    homework for week 4

--- earthquakes.py
+++ earthquakes.py
@@ -3,6 +3,7 @@
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import requests
+import json
 
 
 def get_data():
@@ -23,36 +24,46 @@ def get_data():
 
     # The response we get back is an object with several fields.
     # The actual contents we care about are in its text field:
+    data = response.json()
     text = response.text
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
-    ...
-
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    with open('earthquakes/my_file.json', 'w') as f:
+        json.dump(data, f)
+    
+    with open('earthquakes/my_file_text.json', 'w') as f2:
+        json.dump(text, f2)
+
+    print("finished writing the data to json")
+    return data
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return len(data["features"])
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return earthquake["geometry"]["coordinates"][0:2]
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    loc_mag_dict = {}
+    for earthquake in data["features"]:
+        loc_mag_dict[get_location(earthquake)]=get_magnitude(earthquake)
 
+    max_loc = max(loc_mag_dict, key = loc_mag_dict.get)
+    return loc_mag_dict[max_loc], max_loc
 
 # With all the above functions defined, we can now call them and get the result
 data = get_data()
