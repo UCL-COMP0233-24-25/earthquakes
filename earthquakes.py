bce0b73baee77b9c78cd9abfbbcebfe8df43a0d8
Hruday Kinhikar <156102552+hskinhikar@users.noreply.github.com>
2024-10-27 03:11:16 +0000

    response.txt and response_json.json are created in the process of converting data for easier viewing/verification of data. Number of earthquakes are counted based on metadata. location finding function is dependant on the magnitude finding function since end goal is to find the corresponding location to the earthquake with greatest magnitude.

--- earthquakes.py
+++ earthquakes.py
@@ -3,6 +3,7 @@
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import requests
+import json
 
 
 def get_data():
@@ -24,33 +25,61 @@ def get_data():
     # The response we get back is an object with several fields.
     # The actual contents we care about are in its text field:
     text = response.text
+    
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
+
+    # Read the entire content of response.txt
+
+    # saves response in a text file
+    with open('response.txt','w') as k:
+        k.write(text)
+
+    with open('response.txt', 'r') as text_file:
+        content = text_file.read()
+
+    # Parse JSON string to Python dictionary
+    content_json = json.loads(content)  
+
+    # Encodes python dictionary into a JSON file
+    with open('response_json.json', 'w') as json_file:
+        json.dump(content_json, json_file, indent=4)  # Ensure indentation for readability
+    
+    # loads JSON file
+    with open('response_json.json', 'r') as json_file:
+        loaded_json = json.load(json_file)
     ...
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return loaded_json 
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return data["metadata"]["count"]
 
 
-def get_magnitude(earthquake):
+def get_magnitude(loaded_json):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return [earthquake["properties"]["mag"] for earthquake in loaded_json["features"]]
 
 
-def get_location(earthquake):
+def get_location(loaded_json,magnitude):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    for earthquake in loaded_json["features"]:
+        if earthquake["properties"]["mag"] == magnitude:
+            place_max_mag = earthquake["geometry"]["coordinates"][0:2]
+            break
+    return place_max_mag
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
+    max_magnitude = max(get_magnitude(data))
+    location_max_magnitude = get_location(data,max_magnitude)
+    return max_magnitude,location_max_magnitude
     ...
 
 
